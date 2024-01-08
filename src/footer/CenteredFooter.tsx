6c560f669cd4536b5327585db67044a10a17b516
Moojin Chae <moojin@Moojins-MacBook-Pro.local>
2024-01-08 00:37:29 -0800

    Initial page layout

--- src/footer/CenteredFooter.tsx
+++ src/footer/CenteredFooter.tsx
@@ -1,11 +1,9 @@
 import type { ReactNode } from 'react';
 
 import { FooterCopyright } from './FooterCopyright';
-import { FooterIconList } from './FooterIconList';
 
 type ICenteredFooterProps = {
   logo: ReactNode;
-  iconList: ReactNode;
   children: ReactNode;
 };
 
@@ -13,27 +11,13 @@ const CenteredFooter = (props: ICenteredFooterProps) => (
   <div className="text-center">
     {props.logo}
 
-    <nav>
-      <ul className="navbar mt-5 flex flex-row justify-center text-xl font-medium text-gray-800">
-        {props.children}
-      </ul>
-    </nav>
-
-    <div className="mt-8 flex justify-center">
-      <FooterIconList>{props.iconList}</FooterIconList>
+    <div className="mt-4 text-lg">
+      hello@mockpolar.ai
     </div>
 
-    <div className="mt-8 text-sm">
+    <div className="mt-4 text-sm">
       <FooterCopyright />
     </div>
-
-    <style jsx>
-      {`
-        .navbar :global(li) {
-          @apply mx-4;
-        }
-      `}
-    </style>
   </div>
 );
 
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'MockPolar AI',
+  title: 'MockPolar AI',
+  description: 'MockPolar AI',
   locale: 'en',
 };
