da48e4a67b5dc2218d0ddabe8c6b673a8e699ace
Moojin Chae <moojin@Moojins-MacBook-Pro.local>
2024-01-19 14:26:48 -0800

    add vercel analytics back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import { GoogleAnalytics } from '@next/third-parties/google'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Component {...pageProps} />
+    <Analytics />
     <GoogleAnalytics gaId="G-XED1JJTNZC" />
   </>
 );
